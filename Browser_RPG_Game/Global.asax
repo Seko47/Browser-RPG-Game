6b5fcc10757f817bdada22c3cc80a8b3b9587d81
Seko47 <rafalsekular96@gmail.com>
2019-05-07 17:27:19 +0200

    Add project files.

---
+++ Browser_RPG_Game/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Browser_RPG_Game.MvcApplication" Language="C#" %>
---
+++ Browser_RPG_Game/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Browser_RPG_Game.Startup))]
+namespace Browser_RPG_Game
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
