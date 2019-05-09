4e34c30a93ca78d753e85d65d619e7abb30837f2
Seko47 <rafalsekular96@gmail.com>
2019-05-09 23:13:52 +0200

    Changing the game context

--- Browser_RPG_Game/DAL/GameContext.cs
+++ Browser_RPG_Game/DAL/GameContext.cs
@@ -13,17 +13,10 @@ namespace Browser_RPG_Game.DAL
         public GameContext() : base("DefaultConnection")
         { }
 
-        public DbSet<Models.Attribute> Attributes { get; set; }
         public DbSet<Character> Characters { get; set; }
-        public DbSet<CharacterAttribute> CharacterAttributes { get; set; }
-        public DbSet<CharacterEquipment> CharacterEquipment { get; set; }
-        public DbSet<CharacterItem> CharacterItems { get; set; }
         public DbSet<Config> Configs { get; set; }
         public DbSet<Enemy> Enemies { get; set; }
-        public DbSet<EnemyLocation> EnemyLocations { get; set; }
-        public DbSet<EquipmentSlot> EquipmentSlots { get; set; }
         public DbSet<Item> Items { get; set; }
-        public DbSet<ItemAttribute> ItemAttributes { get; set; }
         public DbSet<ItemLoot> ItemLoots { get; set; }
         public DbSet<ItemType> ItemTypes { get; set; }
         public DbSet<Location> Locations { get; set; }
@@ -31,17 +24,70 @@ namespace Browser_RPG_Game.DAL
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<ProfileType> ProfileTypes { get; set; }
         public DbSet<Bulletin> Bulletins { get; set; }
-        public DbSet<Store> Stores { get; set; }
-        public DbSet<ItemStore> ItemStores { get; set; }
         public DbSet<Property> Properties { get; set; }
         public DbSet<Building> Buildings { get; set; }
         public DbSet<Material> Materials { get; set; }
-        public DbSet<BuildingProperty> BuildingProperties { get; set; }
+        public DbSet<Property> BuildingProperties { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.Helmet)
+                .WithMany(i => i.CharactersHelmets)
+                .HasForeignKey(c => c.HelmetID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.Armor)
+                .WithMany(i => i.CharactersArmors)
+                .HasForeignKey(c => c.ArmorID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.Gloves)
+                .WithMany(i => i.CharactersGloves)
+                .HasForeignKey(c => c.GlovesID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.Legs)
+                .WithMany(i => i.CharactersLegs)
+                .HasForeignKey(c => c.LegsID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.Feet)
+                .WithMany(i => i.CharactersFeet)
+                .HasForeignKey(c => c.FeetID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.MainHand)
+                .WithMany(i => i.CharactersMainHand)
+                .HasForeignKey(c => c.MainHandID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Character>()
+                .HasOptional<Item>(c => c.OffHand)
+                .WithMany(i => i.CharactersOffHand)
+                .HasForeignKey(c => c.OffHandID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Message>()
+                .HasRequired<Character>(m => m.Sender)
+                .WithMany(c => c.SendedMessages)
+                .HasForeignKey(m => m.SenderID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Message>()
+                .HasRequired<Character>(m => m.Receiver)
+                .WithMany(c => c.ReceivedMessages)
+                .HasForeignKey(m => m.ReceiverID)
+                .WillCascadeOnDelete(false);
         }
     }
 }
