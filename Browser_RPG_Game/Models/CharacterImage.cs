6882909dc3b8694069857b44e06c46a50600f306
Seko47 <rafalsekular96@gmail.com>
2019-05-17 11:49:04 +0200

    Improved CRUD for character images

--- Browser_RPG_Game/Models/CharacterImage.cs
+++ Browser_RPG_Game/Models/CharacterImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace Browser_RPG_Game.Models
     public class CharacterImage
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name ="Path to image")]
         public string PathToImage { get; set; }
 
         public virtual ICollection<Character> Characters { get; set; }
