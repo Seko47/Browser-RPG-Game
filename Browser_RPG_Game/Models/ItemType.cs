ade64412e51282a2a2247bd28913b07f08bea95d
Seko47 <rafalsekular96@gmail.com>
2019-05-18 22:56:46 +0200

    Added validation for item type

--- Browser_RPG_Game/Models/ItemType.cs
+++ Browser_RPG_Game/Models/ItemType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Browser_RPG_Game.Models
     public class ItemType
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
