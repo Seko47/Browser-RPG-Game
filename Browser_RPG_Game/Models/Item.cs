77bb89d1cfa507b7a5d765d9dbae36ea321b779c
Seko47 <rafalsekular96@gmail.com>
2019-05-18 22:55:14 +0200

    Improved CRUD for item

--- Browser_RPG_Game/Models/Item.cs
+++ Browser_RPG_Game/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,20 @@ namespace Browser_RPG_Game.Models
     public class Item
     {
         public int ID { get; set; }
+        [Display(Name="Item type")]
         public int ItemTypeID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(0, 1000)]
         public int Level { get; set; }
+        [Range(0, 1000)]
         public int Damage { get; set; }
+        [Range(0, 1000)]
         public int Defense { get; set; }
+        [Range(0, 1000000)]
         public int Value { get; set; }
+        [Required]
+        [Display(Name="Path to image")]
         public string PathToImage { get; set; }
 
         public virtual ItemType ItemType { get; set; }
