b94d6db2698e7823cc7d28db5a814c4a752f96fd
Seko47 <rafalsekular96@gmail.com>
2019-05-18 23:00:43 +0200

    Improved CRUD for material

--- Browser_RPG_Game/Models/Material.cs
+++ Browser_RPG_Game/Models/Material.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace Browser_RPG_Game.Models
     public class Material
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(1, 10000)]
         public int Value { get; set; }
+        [Required]
+        [Display(Name="Path to image")]
         public string PathToImage { get; set; }
     }
 }
