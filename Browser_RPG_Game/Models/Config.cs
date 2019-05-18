5e28589457c4b6510a60b198c126cb0965dae4e5
Seko47 <rafalsekular96@gmail.com>
2019-05-18 22:37:08 +0200

    Added validation for config

--- Browser_RPG_Game/Models/Config.cs
+++ Browser_RPG_Game/Models/Config.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace Browser_RPG_Game.Models
     public class Config
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Value { get; set; }
     }
 }
